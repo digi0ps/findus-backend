302fff29d2fdf22ae8f948779a59fbfc097d3bdf
digi0ps <sriru1998@gmail.com>
2019-10-15 20:10:11 +0530

    Add models.

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from api.models import *
 
 # Register your models here.
+admin.register(FaceEncoding)
+admin.register(Photo)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FaceEncoding(models.Model):
+    id = models.AutoField(primary_key=True)
+    person_name = models.CharField(max_length=50, blank=True, default="Anon")
+    encoding = models.CharField(max_length=9999)
+
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='gallery')
+    person = models.ForeignKey(
+        to=FaceEncoding, on_delete=models.SET_NULL, null=True)
