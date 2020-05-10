29765f784270be22ffa23848b87c3288559edc0f
digi0ps <sriru1998@gmail.com>
2020-05-10 16:41:03 +0530

    Add postgres to docker.

--- root/settings.py
+++ root/settings.py
@@ -81,10 +81,16 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'findus',
+        'USER': 'digi0ps',
+        'PASSWORD': 'passpass',
+        'HOST': 'database',
+        'PORT': '5432',
     }
 }
+
+CONN_MAX_AGE = None
 
 
 # Password validation
